220a31ef63944e3361bafb436872ed7ebf2314fc
Anne <tchokouafe2005@yahoo.fr>
2023-06-30 16:06:00 -0700

    terraform

---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+my_region     = "us-east-1"
+profile       = "default"
+topic_name    = "sns-dev"
+endpoint      = "tchokouafe2018@gmail.com"
+
+protocol_type = "email"
+team          = "dev"
---
+++ version.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.6.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.my_region
+
+  profile = var.profile
+
+}
